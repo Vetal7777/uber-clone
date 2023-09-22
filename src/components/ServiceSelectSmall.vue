c7b01a886560f10b006f7a61a409c9ea84496106
Vetal7777 <jobschildren@gmail.com>
2023-09-22 17:26:19 +0300

    feat: create new components

---
+++ src/components/ServiceSelectSmall.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="ServiceSelectSmall">
+    <div class="bg-custom-color custom-height relative w-full rounded-2xl">
+      <div
+        class="absolute left-1/3 top-1/3 mt-0.5 -translate-x-1/4 -translate-y-1/4 transform"
+      >
+        <img :src="`img/uber/${image}.png`" alt="" width="55" />
+      </div>
+    </div>
+    <div class="mt-2 text-center text-xs text-gray-900">{{ text }}</div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { defineProps, toRefs } from 'vue'
+
+type Props = {
+  text: string
+  image: string
+}
+const props = defineProps<Props>()
+
+const { text, image } = toRefs(props)
+</script>
+
+<style scoped lang="scss">
+#ServiceSelectSmall {
+  .bg-custom-color {
+    background-color: rgb(237, 237, 237);
+  }
+
+  .custom-height {
+    height: 70px;
+  }
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,21 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div id="Home" class="flex h-screen flex-col">
+    <div class="m-3 mb-auto">
+      <div class="my-3 grid grid-cols-2 gap-3">
+        <ServiceSelectLarge text="Ride" image-width="74" image="ride" />
+        <ServiceSelectLarge text="Food" image-width="74" image="food" promo />
+      </div>
+      <div class="my-3 grid grid-cols-4 gap-3">
+        <ServiceSelectSmall text="2 Wheels" image="2wheels" />
+        <ServiceSelectSmall text="Transit" image="transit" />
+        <ServiceSelectSmall text="Reserve" image="reserve" />
+        <ServiceSelectSmall text="Travel" image="travel" />
+      </div>
+    </div>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
-
-export default defineComponent({
-  name: 'HomeView',
-  components: {
-    HelloWorld,
-  },
-});
+<script setup lang="ts">
+import ServiceSelectLarge from '../components/ServiceSelectLarge.vue'
+import ServiceSelectSmall from '../components/ServiceSelectSmall.vue'
 </script>
