46c7c459e5d810037829f24b9a1756013ca7baf7
Vetal7777 <jobschildren@gmail.com>
2023-09-26 18:28:17 +0300

    feat: add inputs with locations

--- src/views/DirectionsView.vue
+++ src/views/DirectionsView.vue
@@ -19,8 +19,33 @@
         <div class="h-5 w-full"></div>
 
         <div class="mb-2 mt-5">
-          <AutoCompleteInput />
+          <AutoCompleteInput
+            the-id="firstInput"
+            v-model:input="pickup"
+            placeholder="Enter pick-up location"
+            @is-active="isPickupActive = true"
+          />
         </div>
+
+        <div class="mb-3">
+          <AutoCompleteInput
+            the-id="secondInput"
+            v-model:input="destination"
+            placeholder="Where to ?"
+            @is-active="isPickupActive = false"
+          />
+        </div>
+      </div>
+    </div>
+
+    <div class="custom-border-bottom flex items-center">
+      <div class="mx-5 my-3.5 rounded-full bg-gray-400 p-1.5">
+        <MapMarkerIcon size="30" fillColor="#f5f5f5" />
+      </div>
+
+      <div>
+        <div class="text-lg text-gray-600">London, UK</div>
+        <div class="text-lg text-gray-400">UK</div>
       </div>
     </div>
   </div>
@@ -30,8 +55,11 @@
 import AutoCompleteInput from '@/components/AutoCompleteInput.vue'
 import { ref } from 'vue'
 import ArrowIcon from 'vue-material-design-icons/ArrowLeft.vue'
+import MapMarkerIcon from 'vue-material-design-icons/MapMarker.vue'
 
 const isPickupActive = ref<boolean>(true)
+const pickup = ref<string>('')
+const destination = ref<string>('')
 </script>
 
 <style scoped lang="scss">
@@ -75,6 +103,10 @@ const isPickupActive = ref<boolean>(true)
     width: 2px;
     height: 45px;
     background-color: rgb(191, 191, 191);
+  }
+
+  .custom-border-bottom {
+    border-bottom: 1px solid rgb(230, 230, 230);
   }
 }
 </style>
