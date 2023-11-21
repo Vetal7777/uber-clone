657a8be3d3b9a101ab41283795a7f6a5dc55d6a7
vetal7777 <jobschildren@gmail.com>
2023-11-21 08:29:00 +0200

    feat: create ServiceSelectLarge component

--- src/components/ServiceSelectLarge.vue
+++ src/components/ServiceSelectLarge.vue
@@ -1,48 +1,62 @@
 <template>
-  <div id="ServiceSelectLarge">
+  <div id="ServiceSetupLarge">
     <div class="bg-custom-color custom-height w-full rounded-2xl">
-      <div v-if="promo" class="relative z-10 w-full">
-        <span
-          class="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 rounded-3xl bg-green-700 p-0.5 px-2 text-xs text-gray-100"
-          >Promo</span
+
+      <div v-if="promo" class="w-full relative z-10">
+        <span 
+          class=
+            "
+              absolute    
+              top-1/2
+              left-1/2
+              transform 
+              -translate-x-1/2 
+              -translate-y-1/2    
+              bg-green-700 
+              p-0.5 
+              px-2 
+              text-gray-100
+              rounded-3xl
+              text-xs
+            "
         >
+          Promo
+        </span>
       </div>
 
       <div class="flex justify-between p-2">
-        <div class="ml-2 mt-14 text-lg text-gray-900">{{ text }}</div>
-
-        <div class="mr-1 mt-3">
-          <img :src="`img/uber/${image}.png`" alt="" :width="imageWidth" />
+        <div class="text-lg ml-2 text-gray-900 mt-14">
+          {{ text }}
+        </div>
+        <div class="mt-3 mr-1">
+          <img :width="imageWidth" :src="'img/uber/' + image + '.png'">
         </div>
       </div>
     </div>
   </div>
 </template>
 
-<script setup lang="ts">
-import { defineProps, toRefs, withDefaults } from 'vue'
+<script setup>
 
-type Props = {
-  text: string
-  imageWidth: string
-  image: string
-  promo: boolean
-}
-const props = withDefaults(defineProps<Props>(), {
-  promo: false
-})
+  import { defineProps, toRefs } from 'vue';
 
-const { text, imageWidth, image, promo } = toRefs(props)
+  const props = defineProps({
+    text: String,
+    imageWidth: String,
+    image: String,
+    promo: { Boolean, default: false }
+  })
+  const { text, imageWidth, image, promo } = toRefs(props)
+
 </script>
 
-<style scoped lang="scss">
-#ServiceSelectLarge {
-  .bg-custom-color {
-    background-color: rgb(237, 237, 237);
+<style lang="scss" scoped>
+  #ServiceSetupLarge {
+    .bg-custom-color {
+      background-color: rgb(237, 237, 237);
+    }
+    .custom-height {
+      height: 100px;
+    }
   }
-
-  .custom-height {
-    height: 100px;
-  }
-}
 </style>
