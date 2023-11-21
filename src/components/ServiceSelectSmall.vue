873428cc92d3bdc6f33819da0a9ed0e8911ad258
vetal7777 <jobschildren@gmail.com>
2023-11-21 08:28:33 +0200

    feat: create ServiceSelectSmall component

--- src/components/ServiceSelectSmall.vue
+++ src/components/ServiceSelectSmall.vue
@@ -1,36 +1,47 @@
 <template>
-  <div id="ServiceSelectSmall">
-    <div class="bg-custom-color custom-height relative w-full rounded-2xl">
-      <div
-        class="absolute left-1/3 top-1/3 mt-0.5 -translate-x-1/4 -translate-y-1/4 transform"
+  <div id="ServiceSetupSmall">
+    <div class="bg-custom-color custom-height w-full rounded-2xl relative">
+
+      <div 
+        class="
+            absolute 
+            top-1/3
+            left-1/3
+            transform 
+            -translate-x-1/4 
+            -translate-y-1/4
+            mt-0.5
+        "
       >
-        <img :src="`img/uber/${image}.png`" alt="" width="55" />
+        <img width="55" :src="'img/uber/' + image + '.png'">
       </div>
     </div>
-    <div class="mt-2 text-center text-xs text-gray-900">{{ text }}</div>
+
+    <div class="text-xs text-center mt-2 text-gray-900">
+        {{ text }}
+    </div>
   </div>
 </template>
 
-<script setup lang="ts">
-import { defineProps, toRefs } from 'vue'
+<script setup>
 
-type Props = {
-  text: string
-  image: string
-}
-const props = defineProps<Props>()
+  import { defineProps, toRefs } from 'vue';
 
-const { text, image } = toRefs(props)
+  const props = defineProps({
+    text: String,
+    image: String
+  })
+  const { text, image } = toRefs(props)
+
 </script>
 
-<style scoped lang="scss">
-#ServiceSelectSmall {
-  .bg-custom-color {
-    background-color: rgb(237, 237, 237);
+<style lang="scss" scoped>
+  #ServiceSetupSmall {
+    .bg-custom-color {
+      background-color: rgb(237, 237, 237);
+    }
+    .custom-height {
+      height: 70px;
+    }
   }
-
-  .custom-height {
-    height: 70px;
-  }
-}
 </style>
