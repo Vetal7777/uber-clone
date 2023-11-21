012ba0a0f9b0ab30554a7fba02ea92f23bf586e8
vetal7777 <jobschildren@gmail.com>
2023-11-21 08:21:54 +0200

    feat: add address and distance endpoints

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const axios = require('axios')
 const cors = require('cors')
 const bodyParser = require('body-parser')
 require('dotenv').config()
-const port = 4001
+const port = process.env.PORT || 4001
 
 axios.defaults.baseURL = process.env.GOOGLE_MAPS_API_URL
 
@@ -12,29 +12,39 @@ app.use(bodyParser.json())
 app.use(cors())
 
 app.get('/api/address/:address', async (req, res) => {
-  try {
-    if (
-      req.params.address === null ||
-      req.params.address === '' ||
-      req.params.address === 'null'
-    ) {
-      return ''
-    }
+    try {
+      if (req.params.address === 'null') return res.status(200).json('');
 
-    let data = await axios.get(
-      'place/autocomplete/json' +
-        '?input=' +
-        req.params.address +
-        '&types=address' +
-        '&key=' +
-        process.env.GOOGLE_MAPS_API_KEY
-    )
-    res.status(200).json(data.data.predictions)
-  } catch (error) {
-    console.log(error)
-  }
+        let data = await axios.get(
+            'place/autocomplete/json' + 
+            '?input=' + req.params.address + 
+            '&types=address' + 
+            '&key=' + process.env.GOOGLE_MAPS_API_KEY
+        )
+
+        res.status(200).json(data.data.predictions);
+      } catch (err) {
+        console.log(err)
+      }
 })
 
+app.get('/api/distance/:pickup/:destination', async (req, res) => {
+    try {
+        let data = await axios.get(
+            'distancematrix/json' + 
+            '?origins=' + req.params.pickup + 
+            '&destinations=' + req.params.destination + 
+            '&units=imperial' + 
+            '&key=' + process.env.GOOGLE_MAPS_API_KEY
+        )
+
+        res.status(200).json(data.data);
+      } catch (err) {
+        console.log(err)
+      }
+})
+
+// Listen to the server
 app.listen(port, () => {
-  console.log(`App listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 })
