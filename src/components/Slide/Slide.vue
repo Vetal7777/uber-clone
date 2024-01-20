9c97c5f010fbd7c34a2aca37717f733eac3e39c9
vetal7777 <jobschildren@gmail.com>
2024-01-20 21:38:00 +0200

    style: hover slide

--- src/components/Slide/Slide.vue
+++ src/components/Slide/Slide.vue
@@ -1,11 +1,13 @@
 <template>
-  <div class="min-w-110 max-w-110 flex cursor-pointer flex-col gap-6">
+  <div class="min-w-110 max-w-110 slide flex cursor-pointer flex-col gap-6">
     <img :src="img" class="w-full rounded-2xl" lazy />
     <div class="flex flex-col gap-5">
       <div class="flex flex-col gap-2">
         <div class="flex gap-3 text-3xl">
           {{ title }}
-          <Icon icon="majesticons:arrow-right" color="black" />
+          <div class="icon transition-all">
+            <Icon icon="majesticons:arrow-right" color="black" />
+          </div>
         </div>
         <div class="text-2xl font-light text-base-gray">
           {{ description }}
@@ -16,10 +18,14 @@
 </template>
 
 <script setup lang="ts">
-import type { SlideProps } from './types'
 import { Icon } from '@iconify/vue'
+import type { SlideProps } from './types'
 
 defineProps<SlideProps>()
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.slide:hover .icon {
+  transform: translateX(10px);
+}
+</style>
